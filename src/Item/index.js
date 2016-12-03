90d23d83564eed4c2dd2afe797d4fae099da01fd
snowling444 <937035581@qq.com>
2016-12-03 16:22:36 +0800

    a

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 var marked = require('marked');
 console.log(marked('I am using __markdown__.'));
 
 class Item extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:''
+    }
+  }
+  componentDidMount(){
+    let address =this.props.params.title;
+    axios.get(`https://raw.githubusercontent.com/snowling444/Limon/master/data/${address}.md`)
+     .then( res => this.setState({data:res.data}) )
+  }
   render () {
-    let content = this.props.params.title == 0 ? 'First' :
-     this.props.params.title == 1 ? 'Second' :'nth'
+    let content = this.state.data.length==0 ? 'Wait' : marked(this.state.data);
     return(
-      <div>
-        {content}
-        <div dangerouslySetInnerHTML={{__html:marked('# HAHA')}}></div>
+      <div className='item-arl'>
+        <div dangerouslySetInnerHTML={{__html:content}} />
       </div>
     )
   }
