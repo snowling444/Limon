cf3b939d64c1d3b693360a188ea07a3f61d7999a
snowling444 <937035581@qq.com>
2016-12-03 15:43:54 +0800

    a

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+var marked = require('marked');
+console.log(marked('I am using __markdown__.'));
 
 class Item extends React.Component {
   render () {
-    let content = this.props.params.title == 1 ? 'First' :
-     this.props.params.title == 2 ? 'Second' :'nth'
+    let content = this.props.params.title == 0 ? 'First' :
+     this.props.params.title == 1 ? 'Second' :'nth'
     return(
       <div>
         {content}
+        <div dangerouslySetInnerHTML={{__html:marked('# HAHA')}}></div>
       </div>
     )
   }
